016177c234c2a4cbeab07901480ee78a6d50ba57
Hadija Trojer <hadija.trojer@gmail.com>
2025-12-07 10:15:25 +0000

    initial commit

---
+++ waybar/.config/waybar/scripts/powermenu.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+chosen=$(echo -e "Suspend\nShutdown\nReboot\nLogout" | wofi --dmenu --width 200 --height 250 --prompt "Power Menu")
+
+case "$chosen" in
+    Suspend)
+        systemctl suspend
+        ;;
+    Shutdown)
+        systemctl poweroff
+        ;;
+    Reboot)
+        systemctl reboot
+        ;;
+    Logout)
+        swaymsg exit
+        ;;
+    *)
+        ;;
+esac
