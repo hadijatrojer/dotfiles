1c349e1d48e869ad23a474c442c07ae7a3ae0358
Hadija Trojer <hadija.trojer@gmail.com>
2025-12-08 21:35:08 +0000

    powermenu

--- waybar/.config/waybar/scripts/powermenu.sh
+++ waybar/.config/waybar/scripts/powermenu.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 
-chosen=$(echo -e "Suspend\nShutdown\nReboot\nLogout" | wofi --dmenu --width 200 --height 250 --prompt "Power Menu")
+chosen=$(echo -e "  Lock\n  Reboot\n󰗼  Logout\n  Shutdown\n⏾  Suspend" | wofi --dmenu --i --width 240 --height 320 --prompt "Power Menu")
 
 case "$chosen" in
-    Suspend)
-        systemctl suspend
+    "  Lock")
+        ~/.config/sway/lock.sh
         ;;
-    Shutdown)
-        systemctl poweroff
-        ;;
-    Reboot)
+    "  Reboot")
         systemctl reboot
         ;;
-    Logout)
+    "󰗼  Logout")
         swaymsg exit
+        ;;
+    "  Shutdown")
+        systemctl poweroff
+        ;;
+    "⏾  Suspend")
+        systemctl suspend
         ;;
     *)
         ;;
