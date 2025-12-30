993d0407ab3ae1866be7710fc57cedef59d00539
Hadija Trojer <hadija.trojer@gmail.com>
2025-12-30 11:16:59 +0000

    newer hypr

--- fedora/install-hyprland.sh
+++ fedora/install-hyprland.sh
@@ -1,14 +1,12 @@
-# https://copr.fedorainfracloud.org/coprs/solopasha/hyprland
+# https://copr.fedorainfracloud.org/coprs/acidburnmonkey/hyprland/
 # https://copr.fedorainfracloud.org/coprs/scottames/ghostty/
-# https://copr.fedorainfracloud.org/coprs/jkinred/satty
 
 # Download the .repo file. there is a download link for YOUR release
-# Copy to yum.repos.d:
+# Copy to /etc/yum.repos.d:
 
 # rpm-ostree refresh-md
 
 rpm-ostree install \
   hyprland waybar xdg-desktop-portal-hyprland hyprpaper hyprlock hypridle \
-  hyprland-qtutils hyprpicker xdg-terminal-exec ghostty wofi pavucontrol \
-  playerctl wiremix slurp grim satty wlr-randr mako \
-  imv fontawesome-fonts-all fastfetch blueman niri
+  hyprland-guiutils xdg-terminal-exec ghostty wofi playerctl wiremix slurp \
+  grim wlr-randr mako imv fontawesome-fonts-all fastfetch blueman niri
